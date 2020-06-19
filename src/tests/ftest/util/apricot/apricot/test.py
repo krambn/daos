d7bd364b4399cc3041d0b738cafb17e7bd310f1e
Brian J. Murrell <brian.murrell@intel.com>
2020-06-19 10:53:07 -0400

    Fix decorator
    Debug spdk setup.sh script
    
    Skip-checkpatch: true
    Quick-build: true
    noRPM-test: true
    noRPM-test-version: 1.1.0-23.4627.g645a066b
    noPR-repos: daos@PR-2060:233
    PR-repos-leap15: lua-lmod@PR-1
    PR-repos: testmpio@PR-4 mpi4py@PR-5
    Skip-run_test: true
    Skip-coverity-test: true
    Signed-off-by: Brian J. Murrell <brian.murrell@intel.com>

--- src/tests/ftest/util/apricot/apricot/test.py
+++ src/tests/ftest/util/apricot/apricot/test.py
@@ -66,9 +66,10 @@ def skipForTicket(ticket, on_distros=[]):
     # see if we are skipping only on some distros and if our distro is one
     # of them
     if on_distros:
-        my_distro = distro.linux_distribution(full_distribution_name=False)
-        if my_distro[0] + '-' + my_distro[1] in on_distros:
-            return False
+        if "-".join(distro.linux_distribution(
+            full_distribution_name=False)[:2]) not in on_distros:
+            # skip doesn't apply on this distro
+            return skip(False)
 
     return skip("Skipping until {} is fixed.".format(ticket))
 # pylint: enable=invalid-name
